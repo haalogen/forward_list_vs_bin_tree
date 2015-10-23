ed70c600938a4ca46296d7e40f7bedcd6fc467ba
stan <citrus_82@nextmail.ru>
2015-10-24 01:45:49 +0300

    Added BinSearchTree implementation and analysis in the problem of frequency dictionary.

--- analysis.py
+++ analysis.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
-    
+#    -George_Orwell
 #    read the text; parse it into words
     text = None
-    fname = '1984_small.txt'
+    fname = '1984-George_Orwell.txt'
     with open(fname, 'r') as f:
         text = f.read().lower()
     
@@ -33,13 +33,27 @@ def main():
     fwd_list_stat.sort(reverse=True)
         
     
-    count_arr = []
+    count_arr_fwd = []
     for item in fwd_list_stat:
-        count_arr.append(item[0])
+         count_arr_fwd.append(item[0])
     
     
 #    2) fill the bin_tree
+    bin_tree = fu.BinarySearchTree()
     
+    for word in words:
+        bin_tree.put(word)
+    
+    bin_tree_stat = bin_tree.get_all_nodes()
+    
+    bin_tree_stat.sort(reverse=True)
+    
+    count_arr_tree = []
+    for item in bin_tree_stat:
+        count_arr_tree.append(item[0])
+    
+    equal = (bin_tree_stat == fwd_list_stat)
+    print "Fwd_list and Bin_tree are equivalent:", equal
     
     
     
@@ -48,7 +62,7 @@ def main():
 #    plot histogram from fwd_list
     fig_list = plt.figure()
     ax_list = fig_list.add_subplot(111)
-    ax_list.plot(range(len(count_arr)), count_arr)
+    ax_list.plot(range(len(count_arr_fwd)),  count_arr_fwd)
     
     label = "FwdList. Top 20 most popular words:\n"
     for i in range(20):
@@ -60,13 +74,26 @@ def main():
     y = 0.2 * (limits[2] + limits[3])
     ax_list.text(x, y, label, fontsize=10)
 
-#    plt.show()
+
 #    plot histogram from bin_tree
+    fig_tree = plt.figure()
+    ax_tree = fig_tree.add_subplot(111)
+    ax_tree.plot(range(len(count_arr_tree)), count_arr_tree, color='r')
     
+    label2 = "BinTree. Top 20 most popular words:\n"
+    for i in range(20):
+        label2 += str(bin_tree_stat[i]) + '\n'
+    label2 += '\n'
+    
+    limits = ax_tree.axis()
+    x = 0.6 * (limits[0] + limits[1])
+    y = 0.2 * (limits[2] + limits[3])
+    ax_tree.text(x, y, label2, fontsize=10)
     
     # saving plots
-    fig_list.savefig("fwd_list_%r_words" % len(count_arr))
-
+    fig_list.savefig("fwd_list_%r_words" % len(count_arr_fwd))
+    fig_tree.savefig("bin_tree_%r_words" % len(count_arr_tree))
+    
 if __name__ == '__main__':
     main()
 
--- func.py
+++ func.py
@@ -1,4 +1,6 @@
 """Basic functionality of the project "Forward_list_vs_Bin_tree"."""
+glob_output = [] # global list for returning bin_tree nodes as list
+
 class Node(object):
     def __init__(self, initdata):
         self.data = initdata
@@ -72,11 +74,63 @@ class FwdList(object):
 
 # bin_tree Class
 class TreeNode(object):
-    def __init__(self, key, val, left=None, right=None):
+    def __init__(self, key, val, left=None, right=None, parent=None):
         self.key = key
-        self.payload = val
+        self.data = val
         self.leftChild = left
         self.rightChild = right
+        self.parent = parent
+        
+    
+    def has_left_child(self):
+        return self.leftChild
+    
+    def has_right_child(self):
+        return self.rightChild
+    
+    def is_left_child(self):
+        return self.parent and self.parent.leftChild == self
+    
+    def is_right_child(self):
+        return self.parent and self.parent.rightChild == self
+    
+    def is_root(self):
+        return not self.parent
+    
+    def is_leaf(self):
+        return not (self.leftChild or self.rightChild)
+    
+    def has_any_children(self):
+        return self.leftChild or self.rightChild
+    
+    def has_both_children(self):
+        return self.leftChild and self.rightChild
+    
+    
+    def replace_node_data(self, key, value, lc, rc):
+        self.key = key
+        self.data = value
+        self.leftChild = lc
+        self.rightChild = rc
+        
+        if self.has_left_child():
+            self.leftChild.parent = self
+            
+        if self.has_right_child():
+            self.rightChild.parent = self
+    
+    
+    def print_tree_node(self, offset):
+        current = self
+        
+        if current.has_left_child():
+            current.leftChild.print_tree_node(offset + 1)
+                
+        if current.has_right_child():
+            current.rightChild.print_tree_node(offset + 1)
+            
+        print "." * offset, current.key, current.data
+        
         
 
 
@@ -87,8 +141,67 @@ class BinarySearchTree(object):
         self.size = 0
     
     
-    def length(self):
-        return self.size
+    
+    def put(self, key, val=1):
+        if self.root:
+            self._put(key, val, self.root)
+        
+        else:
+            self.root = TreeNode(key, val)
+        self.size = self.size + 1
+    
+    
+    def _put(self, key, val, currentNode):
+        if key == currentNode.key:
+            currentNode.data += 1
+            
+        elif key < currentNode.key:
+            if currentNode.has_left_child():
+                self._put(key, val, currentNode.leftChild)
+                
+            else:
+                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
+        else:
+            if currentNode.has_right_child():
+                self._put(key, val, currentNode.rightChild)
+                
+            else:
+                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
+        
+    
+    
+    def print_tree(self, offset=0):
+        current = self.root
+        
+        if current != None:
+            if current.has_left_child():
+                current.leftChild.print_tree_node(offset + 1)
+                
+            if current.has_right_child():
+                current.rightChild.print_tree_node(offset + 1)
+            
+            print "." * offset, current.key, current.data
+        
+    
+    def get_all_nodes(self):
+        
+        if self.root:
+            output = self._get_all_nodes(self.root)
+        
+        return output
+    
+    
+    def _get_all_nodes(self, currentNode):
+        global glob_output
+        
+        if currentNode.has_left_child():
+            self._get_all_nodes(currentNode.leftChild)
+            
+        if currentNode.has_right_child():
+            self._get_all_nodes(currentNode.rightChild)
+        
+        glob_output.append([currentNode.data, currentNode.key])
+        return glob_output
 
 
 
