82040ae3b8b42709b37a8d8a20a836081b566b4d
stan <citrus_82@nextmail.ru>
2015-10-23 22:04:48 +0300

    Implemented FwdList and collecting of statistics of words' frequencies in the text.

--- analysis.py
+++ analysis.py
@@ -1,8 +1,71 @@
 import func as fu
-
+import re
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    pass
+    
+#    read the text; parse it into words
+    text = None
+    fname = '1984_small.txt'
+    with open(fname, 'r') as f:
+        text = f.read().lower()
+    
+#    replaces anything that is not a lowercase letter, space or 
+#    apostrophe with a space
+    text = re.sub('[^a-z]+', " ", text)
+    words = text.split()
+    
+#    1) fill the fwd_list
+    fwd_list = fu.FwdList()
+    
+    for word in words:
+        fwd_list.add(word)
+    
+    
+    fwd_list_stat = []
+    current = fwd_list.head
+    while current != None:
+        fwd_list_stat.append([current.getCount(), current.getData()])
+        current = current.getNext()
+    
+    
+    fwd_list_stat.sort(reverse=True)
+        
+    
+    count_arr = []
+    for item in fwd_list_stat:
+        count_arr.append(item[0])
+    
+    
+#    2) fill the bin_tree
+    
+    
+    
+    
+    
+    
+#    plot histogram from fwd_list
+    fig_list = plt.figure()
+    ax_list = fig_list.add_subplot(111)
+    ax_list.plot(range(len(count_arr)), count_arr)
+    
+    label = "FwdList. Top 20 most popular words:\n"
+    for i in range(20):
+        label += str(fwd_list_stat[i]) + '\n'
+    label += '\n'
+    
+    limits = ax_list.axis()
+    x = 0.6 * (limits[0] + limits[1])
+    y = 0.2 * (limits[2] + limits[3])
+    ax_list.text(x, y, label, fontsize=10)
+
+#    plt.show()
+#    plot histogram from bin_tree
+    
+    
+    # saving plots
+    fig_list.savefig("fwd_list_%r_words" % len(count_arr))
 
 if __name__ == '__main__':
     main()
--- func.py
+++ func.py
@@ -1,3 +1,109 @@
-"""
-Basic functionality of the project "Forward_list_vs_Bin_tree".
-"""
+"""Basic functionality of the project "Forward_list_vs_Bin_tree"."""
+class Node(object):
+    def __init__(self, initdata):
+        self.data = initdata
+        self.count = 1
+        self.next = None
+    
+    def getData(self):
+        return self.data
+    
+    def getNext(self):
+        return self.next
+    
+    def getCount(self):
+        return self.count
+    
+    def setData(self, newdata):
+        self.data = newdata
+    
+    def setNext(self, newnext):
+        self.next = newnext
+    
+    def incCount(self):
+        self.count += 1
+    
+
+
+
+# fwd_list Class
+class FwdList(object):
+    def __init__(self):
+        self.head = None
+    
+    
+    def add(self, item):
+        current = self.head
+        previous = None
+        stop = False
+        
+        while current != None and not stop:
+            if current.getData() == item:
+                current.incCount()
+                stop = True
+                return
+                
+            elif current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+                
+        
+        temp = Node(item)
+        if previous == None:    # insert before first
+            temp.setNext(self.head)
+            self.head = temp
+            
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+    
+    
+    def print_list(self):
+        current = self.head
+        
+        while current != None and current.getNext != None:
+            print current.getData(), current.getCount()
+            current = current.getNext()
+        
+
+
+
+# bin_tree Class
+class TreeNode(object):
+    def __init__(self, key, val, left=None, right=None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        
+
+
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+        self.size = 0
+    
+    
+    def length(self):
+        return self.size
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
